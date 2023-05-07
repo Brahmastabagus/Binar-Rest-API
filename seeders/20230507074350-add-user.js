ebad2c72151973265729570fc81c67cead5f2390
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-05-07 14:50:42 +0700

    Add seeder

---
+++ seeders/20230507074350-add-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('users', [{
+      username: "test",
+      password: "test123",
+      role: "admin",
+      createdAt: "2023-05-02T14:00:07.369Z",
+      updatedAt: "2023-05-06T10:08:51.886Z"
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
